1ac5ce7b68b70a6dee603f7f4538a1455e95e784
RuairiOKeefe <RuairiOkeefe17@gmail.com>
2018-05-22 02:53:23 +0100

    Started entity component implementation

---
+++ Fredrick/src/Entity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Fredrick
+{
+
+
+	abstract class Component : Transform
+	{
+		protected bool active;
+		protected Entity parent;
+
+		public bool GetActive() { return active; }
+
+		public abstract void Update(double delta);
+		public abstract void Draw();
+	}
+
+	class Entity : Transform
+	{
+		private bool active;
+		Dictionary<String, Component> components;
+
+		public bool GetActive() { return active; }
+
+		public void Update(double delta)
+		{
+			foreach (Component c in components.Values)
+			{
+				c.Update(delta);
+			}
+		}
+
+		public void Draw()
+		{
+			foreach (Component c in components.Values)
+			{
+				c.Draw();
+			}
+		}
+	}
+}
