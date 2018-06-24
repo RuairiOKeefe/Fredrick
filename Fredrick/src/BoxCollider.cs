5ffd1198c0462207c919eddb68adce79b245ad7e
RuairiOKeefe <RuairiOkeefe17@gmail.com>
2018-06-24 04:16:51 +0100

    Fixed jittering and removed irrelevant code for collisions

--- Fredrick/src/BoxCollider.cs
+++ Fredrick/src/BoxCollider.cs
@@ -34,51 +34,43 @@ namespace Fredrick.src
 		{
 			Vector2 move = _owner.GetComponent<Character>().GetMove();
 			Vector2 tempMove = move;
-			for (int i = 0; i < 2; i++)
-			{
-				if (i == 0)
-				{
-					//if i = 0 try horizontal collision,
-					Vector2 testMove = new Vector2(move.X, 0);
-					Vector2 newPos = ((_owner.GetPosition() + GetPosition() + testMove));
+			//if i = 0 try horizontal collision,
+			Vector2 testMove = new Vector2(move.X, 0);
+			Vector2 newPos = ((_owner.GetPosition() + GetPosition() + testMove));
 
-					_rectangle.UpdatePosition(newPos);
-					if (_rectangle.Intersect(other._rectangle))
-					{
-						if (_owner.GetComponent<Character>().Velocity.X > 0)
-						{
-							tempMove.X -= _rectangle.GetMax().X - other._rectangle.GetMin().X;
-						}
-						else
-							if (_owner.GetComponent<Character>().Velocity.X < 0)
-						{
-							tempMove.X -= _rectangle.GetMin().X - other._rectangle.GetMax().X;
-						}
-						_owner.GetComponent<Character>().StopVelX();
-					}
+			_rectangle.UpdatePosition(newPos);
+			if (_rectangle.Intersect(other._rectangle))
+			{
+				if (_owner.GetComponent<Character>().Velocity.X > 0)
+				{
+					tempMove.X -= _rectangle.GetMax().X - other._rectangle.GetMin().X;
 				}
 				else
+					if (_owner.GetComponent<Character>().Velocity.X < 0)
 				{
-					Vector2 testMove = new Vector2(0, move.Y);
-					Vector2 newPos = ((_owner.GetPosition() + GetPosition() + testMove));
-
-					_rectangle.UpdatePosition(newPos);
-					if (_rectangle.Intersect(other._rectangle))
-					{
-						if (_owner.GetComponent<Character>().Velocity.Y > 0)
-						{
-							tempMove.Y -= _rectangle.GetMin().Y - other._rectangle.GetMax().Y;
-						}
-						else
-							if (_owner.GetComponent<Character>().Velocity.Y < 0)
-						{
-							tempMove.Y -= _rectangle.GetMax().Y - other._rectangle.GetMin().Y;
-						}
-						_owner.GetComponent<Character>().StopVelY();
-					}
+					tempMove.X -= _rectangle.GetMin().X - other._rectangle.GetMax().X;
 				}
-				_owner.Move(tempMove);
+				_owner.GetComponent<Character>().StopVelX();
 			}
+
+			testMove = new Vector2(0, move.Y);
+			newPos = ((_owner.GetPosition() + GetPosition() + testMove));
+
+			_rectangle.UpdatePosition(newPos);
+			if (_rectangle.Intersect(other._rectangle))
+			{
+				if (_owner.GetComponent<Character>().Velocity.Y > 0)
+				{
+					tempMove.Y -= _rectangle.GetMin().Y - other._rectangle.GetMax().Y;
+				}
+				else
+					if (_owner.GetComponent<Character>().Velocity.Y < 0)
+				{
+					tempMove.Y -= _rectangle.GetMax().Y - other._rectangle.GetMin().Y;
+				}
+				_owner.GetComponent<Character>().StopVelY();
+			}
+			_owner.Move(tempMove);
 		}
 
 		public override void Draw(SpriteBatch spriteBatch)
